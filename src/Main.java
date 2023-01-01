630c4616c28c553884084e6707d71fb5c97b5475
Naitik Sharma <snaitik104@gmail.com>
2023-01-01 12:46:51 +0530

    day-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import microsoft.evaluateReversePolishNotation;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        evaluateReversePolishNotation erpn = new evaluateReversePolishNotation();
     }
 }
--- src/microsoft/evaluateReversePolishNotation.java
+++ src/microsoft/evaluateReversePolishNotation.java
@@ -2,17 +2,18 @@ package microsoft;
 import java.util.Stack;
 public class evaluateReversePolishNotation {
 
-        public int evalRPN(String[] tokens) {
-            Stack<Integer> st = new Stack<>();
-            for(String t : tokens){
-                if("+-*/".contains(t))
-                    st.push(evaluation(st.pop(), st.pop(), t));
-                else
-                    st.push(Integer.parseInt(t));
-            }
-            return st.pop();
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+        for (String t : tokens) {
+            if ("+-*/".contains(t))
+                st.push(eval(st.pop(), st.pop(), t));
+            else
+                st.push(Integer.parseInt(t));
         }
-        private int evaluation(int b, int a, String op){
+        return st.pop();
+    }
+
+        public int eval(int b, int a, String op){
             if("+".equals(op))
                 return a+b;
             else if("-".equals(op))
